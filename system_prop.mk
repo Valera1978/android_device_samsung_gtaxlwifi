5a4230a5c32fa86d2576e8a8d6aca45d66f37a8c
Jean-Michel Trivi <jmtrivi@google.com>
2017-06-22 19:20:26 -0700

    gtaxl: Media volume: from 15 to 25 steps
    Test: play media, use all 25 vol steps and observe varying attenuation
    Bug: 32195657
    Change-Id: I7ac87df0b2b15de3a7bb87e739bd88ef7b185236

--- system_prop.mk
+++ system_prop.mk
@@ -1,7 +1,8 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
-    audio_hal.force_voice_config=wide
+    audio_hal.force_voice_config=wide \
+    ro.config.media_vol_steps=25
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
