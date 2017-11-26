5c5b597e262173c8983764457426abe1a32e00e1
Valera1978 <valera1978@tut.by>
2017-11-26 11:58:11 +0300

    update setup-makefiles.sh to 32bits

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -29,25 +29,20 @@ LOCAL_PATH := \$(call my-dir)
 include \$(CLEAR_VARS)
 LOCAL_MODULE := libGLES_mali
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/egl/libGLES_mali.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/egl/libGLES_mali.so
-LOCAL_MULTILIB := both
+LOCAL_SRC_FILES := proprietary/vendor/lib/egl/libGLES_mali.so
+LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_PATH_32 := \$(\$(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_VENDOR_SHARED_LIBRARIES)/egl
-LOCAL_MODULE_PATH_64 := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/egl
+LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/egl
 
 SYMLINKS := \$(TARGET_OUT)/vendor
 \$(SYMLINKS):
 	@echo "Symlink: vulkan.exynos5.so"
 	@mkdir -p \$@/lib/hw
-	@mkdir -p \$@/lib64/hw
 	\$(hide) ln -sf ../egl/libGLES_mali.so \$@/lib/hw/vulkan.exynos5.so
-	\$(hide) ln -sf ../egl/libGLES_mali.so \$@/lib64/hw/vulkan.exynos5.so
 	@echo "Symlink: libOpenCL.so.1.1"
 	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so.1.1
-	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib64/libOpenCL.so.1.1
 
 ALL_MODULES.\$(LOCAL_MODULE).INSTALLED := \\
 	\$(ALL_MODULES.\$(LOCAL_MODULE).INSTALLED) \$(SYMLINKS)
