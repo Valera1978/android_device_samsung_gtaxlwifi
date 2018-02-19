fc0ff6ac7add14431fcf06ffbe8573a8536875a4
Valera1978 <valera1978@tut.by>
2018-02-19 12:42:17 +0300

    add shims for camera

---
+++ libshims/libshim_camera/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := libshim_camera.cpp
+LOCAL_SHARED_LIBRARIES := libui
+LOCAL_MODULE := libshim_camera
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
