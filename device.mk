42a451473e6a19a0dc55b46fd722bd2790bc56aa
Valera1978 <valera1978@tut.by>
2018-03-02 11:04:37 +0300

    remove libstagefright_shim

--- device.mk
+++ device.mk
@@ -92,8 +92,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libExynosOMX_shim \
-    libstagefright_shim \
     libui_shim \
+
+#    libstagefright_shim \
 
 # Overriden service definition
 PRODUCT_COPY_FILES += \
@@ -116,7 +117,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libshim_camera \
-    libexynoscamera_shim \
+    libexynoscamera_shim
+
+PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.device@1.0-impl \
     android.hardware.camera.provider@2.4-impl
