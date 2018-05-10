686ff142a6565a4961d1f59176e2d0c2afb96741
Valera1978 <valera1978@tut.by>
2018-05-10 21:13:20 +0300

    add power.rc

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -77,3 +77,11 @@ LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := ueventd.samsungexynos7870.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.power.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := BOOT
+LOCAL_SRC_FILES    := init.power.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
