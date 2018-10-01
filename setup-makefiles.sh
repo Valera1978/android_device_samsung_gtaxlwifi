98ae023c6cfb4370518e9fb5d2cc62f81c75cba5
Stricted <info@stricted.net>
2018-10-01 07:14:02 +0200

    gtaxlwifi: Create vulkan and openCL symlinks
    Change-Id: Id79b8fbc0f9ec65de4900366022aa5cd72475694

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
 
 VENDOR=samsung
 DEVICE=gtaxlwifi
@@ -42,6 +41,56 @@ write_headers "gtaxlwifi"
 
 # The standard blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt true
+
+###################################################################################################
+# CUSTOM PART START                                                                               #
+###################################################################################################
+OUTDIR=vendor/$VENDOR/$DEVICE
+(cat << EOF) >> $LINEAGE_ROOT/$OUTDIR/Android.mk
+include \$(CLEAR_VARS)
+LOCAL_MODULE := libGLES_mali
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/egl/libGLES_mali.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/egl/libGLES_mali.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_PATH_32 := \$(\$(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_VENDOR_SHARED_LIBRARIES)/egl
+LOCAL_MODULE_PATH_64 := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/egl
+
+SYMLINKS := \$(TARGET_OUT)/vendor
+\$(SYMLINKS):
+	@echo "Symlink: vulkan.exynos5.so"
+	@mkdir -p \$@/lib/hw
+	@mkdir -p \$@/lib64/hw
+	\$(hide) ln -sf ../egl/libGLES_mali.so \$@/lib/hw/vulkan.exynos5.so
+	\$(hide) ln -sf ../egl/libGLES_mali.so \$@/lib64/hw/vulkan.exynos5.so
+	@echo "Symlink: libOpenCL.so"
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib64/libOpenCL.so
+	@echo "Symlink: libOpenCL.so.1"
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so.1
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib64/libOpenCL.so.1
+	@echo "Symlink: libOpenCL.so.1.1"
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so.1.1
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib64/libOpenCL.so.1.1
+
+ALL_MODULES.\$(LOCAL_MODULE).INSTALLED := \\
+	\$(ALL_MODULES.\$(LOCAL_MODULE).INSTALLED) \$(SYMLINKS)
+
+include \$(BUILD_PREBUILT)
+
+EOF
+
+(cat << EOF) >> $LINEAGE_ROOT/$OUTDIR/$DEVICE-vendor.mk
+
+# Create Mali links for Vulkan and OpenCL
+PRODUCT_PACKAGES += libGLES_mali
+EOF
+###################################################################################################
+# CUSTOM PART END                                                                                 #
+###################################################################################################
 
 # Done
 write_footers
