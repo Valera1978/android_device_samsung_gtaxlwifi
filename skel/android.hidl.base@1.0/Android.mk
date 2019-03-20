1337016a4a4fd8bb85d7d6fffa67bc015c676358
Valera1978 <valera1978@tut.by>
2019-03-20 22:41:47 +0300

    update hild.base

--- skel/android.hidl.base@1.0/Android.mk
+++ skel/android.hidl.base@1.0/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018 TeamNexus
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,17 +12,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidltransport
+
+LOCAL_SRC_FILES := \
+	android.hidl.base@1.0.cpp
+
 LOCAL_MODULE := android.hidl.base@1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include $(BUILD_SHARED_LIBRARY)
+LOCAL_VENDOR_MODULE := true
 
-include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidltransport
-LOCAL_MODULE := android.hidl.manager@1.0
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
