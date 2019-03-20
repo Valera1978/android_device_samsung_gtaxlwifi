615f12d7d8a0b17ec59d44526a8789077a42bc46
Valera1978 <valera1978@tut.by>
2019-03-20 22:44:58 +0300

    add fingerprint (thanks @followmsi)

--- lineage_gtaxllte.mk
+++ lineage_gtaxllte.mk
@@ -30,4 +30,15 @@ PRODUCT_DEVICE := gtaxllte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T585
 PRODUCT_MANUFACTURER := samsung
+
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="gtaxllte" \
+    PRODUCT_NAME="gtaxllte" \
+    PRIVATE_BUILD_DESC="gtaxlltexx-user 8.1.0 M1AJQ T585XXU4CRK5 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Xiaomi/sagit/sagit:7.1.1/NMF26X/V8.2.17.0.NCACNEC:user/release-keys
+
+TARGET_VENDOR := Samsung
