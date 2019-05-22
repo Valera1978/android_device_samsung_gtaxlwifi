91703481c49ce398bf76926d5af7ccf380ae93f4
Valera1978 <valera1978@tut.by>
2019-05-22 20:18:24 +0300

    rollback SamsungDoze

--- device.mk
+++ device.mk
@@ -115,6 +115,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Doze
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl.exynos7870 \
