5c01cbf1ba46348c39df39d65d6e33e083785265
Benedict Wong <benedictwong@google.com>
2019-07-16 12:57:20 -0700

    gtaxl-common: Add feature flag for FEATURE_IPSEC_TUNNELS
    This commit adds the IPSEC_TUNNELS feature flag, with XFRM Interfaces
    used as the implementation of choice.
    
    Bug: 117183273
    Bug: 137629222
    Test: Compiles, passes tunnel-mode CTS tests
    Test: adb shell pm list features
    Change-Id: I19c598947525783850b29d149db85db89f8b347e

--- device.mk
+++ device.mk
@@ -63,6 +63,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
