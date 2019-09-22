1fd78ac575f1ccc20008f0f6367779c8bfab3e68
Valera1978 <valera1978@tut.by>
2019-09-22 19:33:45 +0300

    update cas

--- device.mk
+++ device.mk
@@ -118,6 +118,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Cas
+PRODUCT_PACKAGES += \
+    android.hardware.cas@1.1-service
+
 # Doze
 PRODUCT_PACKAGES += \
     SamsungDoze
