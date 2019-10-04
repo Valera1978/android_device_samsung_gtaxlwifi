a226cb0fabb809777e00b82551efcd88ab1f2903
Valera1978 <valera1978@tut.by>
2019-10-04 10:19:25 +0300

    fix OpenCL links

--- Android.mk
+++ Android.mk
@@ -21,11 +21,11 @@ ifneq ($(filter gtaxllte, $(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 # Create links for OpenCL files
-$(shell mkdir -p  $(TARGET_OUT)/vendor/lib; \
-	mkdir -p  $(TARGET_OUT)/vendor/lib/hw; \
-    ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT)/vendor/lib/libOpenCL.so.1.1; \
-    ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT)/vendor/lib/libOpenCL.so.1; \
-    ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT)/vendor/lib/libOpenCL.so; \
-    ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT)/vendor/lib/hw/vulkan.exynos5.so)
+$(shell mkdir -p  $(TARGET_COPY_OUT_VENDOR)/lib; \
+	mkdir -p  $(TARGET_COPY_OUT_VENDOR)/lib/hw; \
+    ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_COPY_OUT_VENDOR)/lib/libOpenCL.so.1.1; \
+    ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_COPY_OUT_VENDOR)/lib/libOpenCL.so.1; \
+    ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_COPY_OUT_VENDOR)/lib/libOpenCL.so; \
+    ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_COPY_OUT_VENDOR)/lib/hw/vulkan.exynos5.so)
 
 endif
