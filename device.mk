e1103d8ce1ffa36ec288c95d7714cf7e27511d66
Valera1978 <valera1978@tut.by>
2019-10-07 22:46:16 +0300

    back to stock sensors

--- device.mk
+++ device.mk
@@ -284,9 +284,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
-    sensors.universal7870
+    android.hardware.sensors@1.0-impl
 
 # Trust HAL
 PRODUCT_PACKAGES += \
