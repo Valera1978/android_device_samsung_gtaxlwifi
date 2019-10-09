320323b8ad8e0a5747b59f4ae6c77330c5e7ec9f
followmsi <followmsi@gmail.com>
2019-10-09 09:47:38 +0200

    gtaxl: Remove DRM service

--- device.mk
+++ device.mk
@@ -110,8 +110,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-impl
 
 # Keymaster
 PRODUCT_PACKAGES += \
