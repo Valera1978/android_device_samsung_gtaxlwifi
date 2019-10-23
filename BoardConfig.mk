afeea82db4763f9f8a399fd01137e7105c515894
followmsi <followmsi@gmail.com>
2019-10-23 12:14:22 +0200

    gtaxl: DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -232,10 +232,11 @@ ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
     endif
   endif
 endif
-DONT_DEXPREOPT_PREBUILTS := true
+# DONT_DEXPREOPT_PREBUILTS := true
 
 # Recovery
 #RECOVERY_VARIANT := twrp
