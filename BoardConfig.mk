57979742ca89d0dfc97d9e3c5aeaa64e587b9007
followmsi <followmsi@gmail.com>
2019-11-10 20:43:59 +0100

    gtaxl: Remove DEXPREOPT

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -224,17 +224,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
 # Ramdisk
 BOARD_ROOT_EXTRA_FOLDERS := efs
 
-# Dexpreopt
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
-# DONT_DEXPREOPT_PREBUILTS := true
-
 # Recovery
 #RECOVERY_VARIANT := twrp
 BOARD_HAS_DOWNLOAD_MODE := true
