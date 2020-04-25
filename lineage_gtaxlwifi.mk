d71c105e5c871730b4997744a00d467ef5811984
TALU <taluemail272@gmail.com>
2020-04-25 11:37:30 +0100

    lineage_gtaxlwifi: update private build description to T580XXU4CSA1.
    Change-Id: I818e6f7ec9cff006bb951f1f44707babc17acb21

--- lineage_gtaxlwifi.mk
+++ lineage_gtaxlwifi.mk
@@ -36,7 +36,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="gtaxlwifi" \
     PRODUCT_NAME="gtaxlwifi" \
-    PRIVATE_BUILD_DESC="gtaxlwifixx-user 8.1.0 M1AJQ T580XXU4CRK5 release-keys"
+    PRIVATE_BUILD_DESC="gtaxlwifixx-user 8.1.0 M1AJQ T580XXU4CSA1 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := Xiaomi/sagit/sagit:7.1.1/NMF26X/V8.2.17.0.NCACNEC:user/release-keys
