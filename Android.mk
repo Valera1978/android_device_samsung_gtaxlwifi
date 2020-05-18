4342c6549f6860e1133b4bcec19f81aa7e5d22d1
Jan Altensen <info@stricted.net>
2020-05-18 12:28:10 +0200

    gtaxl-common: generate vulkan and opencl symlinks
    [ TALU - and get rid of the old method of doing so ]
    
    Change-Id: I25501e0ffcb86b8e21ee44c648b9f14bbb931b8b

--- Android.mk
+++ Android.mk
@@ -20,4 +20,24 @@ ifneq ($(filter gtaxlwifi, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+include $(CLEAR_VARS)
+
+EGL_LIBS := libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 vulkan.exynos5.so
+
+EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(EGL_LIBS))
+$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 32-bit lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib/egl/libGLES_mali.so $@
+
+EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(EGL_LIBS))
+$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 64-bit lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS)
+
 endif
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -33,56 +33,5 @@ write_headers "gtaxlwifi gtanotexlwifi"
 
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
-###################################################################################################
-# CUSTOM PART START                                                                               #
-###################################################################################################
-OUTDIR=vendor/$VENDOR/$DEVICE
-(cat << EOF) >> $ANDROID_ROOT/$OUTDIR/Android.mk
-include \$(CLEAR_VARS)
-LOCAL_MODULE := libGLES_mali
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/egl/libGLES_mali.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/egl/libGLES_mali.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_CHECK_ELF_FILES := false
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_PATH_32 := \$(\$(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_VENDOR_SHARED_LIBRARIES)/egl
-LOCAL_MODULE_PATH_64 := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/egl
-
-SYMLINKS := \$(TARGET_OUT)/vendor
-\$(SYMLINKS):
-	@mkdir -p \$@/lib/hw
-	@mkdir -p \$@/lib64/hw
-	@echo "Symlink: vulkan.exynos5.so"
-	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/vulkan.exynos5.so
-	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib64/vulkan.exynos5.so
-	@echo "Symlink: libOpenCL.so"
-	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so
-	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib64/libOpenCL.so
-	@echo "Symlink: libOpenCL.so.1"
-	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so.1
-	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib64/libOpenCL.so.1
-	@echo "Symlink: libOpenCL.so.1.1"
-	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so.1.1
-	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib64/libOpenCL.so.1.1
-
-ALL_MODULES.\$(LOCAL_MODULE).INSTALLED := \\
-	\$(ALL_MODULES.\$(LOCAL_MODULE).INSTALLED) \$(SYMLINKS)
-
-include \$(BUILD_PREBUILT)
-
-EOF
-
-(cat << EOF) >> $ANDROID_ROOT/$OUTDIR/$DEVICE-vendor.mk
-
-# Create Mali symlinks for Vulkan and OpenCL
-PRODUCT_PACKAGES += libGLES_mali
-EOF
-###################################################################################################
-# CUSTOM PART END                                                                                 #
-###################################################################################################
-
 # Finish
 write_footers
