17500d60c256116f8665fcd6700788c4f819e526
Valera1978 <valera1978@tut.by>
2020-05-28 15:09:14 +0300

    switch to samsung audio

--- device.mk
+++ device.mk
@@ -219,12 +219,16 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     libtinycompress \
 
-PRODUCT_PACKAGES += \
-    audio.primary.universal7870_32
+#PRODUCT_PACKAGES += \
+#    audio.primary.universal7870_32
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
+#    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
