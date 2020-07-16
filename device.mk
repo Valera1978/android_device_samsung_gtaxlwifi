e293852807421a040b843b101ec94209739ad184
TALU <taluemail272@gmail.com>
2020-07-16 14:09:34 +0100

    gtaxlwifi: make a few changes for GPS.
    Change-Id: I02bb96786221f64755cd230966008e891dd551c4

--- device.mk
+++ device.mk
@@ -246,7 +246,7 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps/gps.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/gps.cfg \
+    $(LOCAL_PATH)/configs/gps/gps_debug.conf:system/etc/gps_debug.conf \
     $(LOCAL_PATH)/configs/gps/gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gps.xml
 
 PRODUCT_PACKAGES += \
