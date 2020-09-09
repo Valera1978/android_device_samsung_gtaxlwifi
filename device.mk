9f81c63679836471e055ec74b88d9d48d5c62ef2
TALU <taluemail272@gmail.com>
2020-09-09 23:21:19 +0100

    gtaxl: revert "revert "Revert "update graphics""".
    This reverts commit e4c72dea2c8a901d04164af9131c2d1985a9957c.

--- device.mk
+++ device.mk
@@ -91,11 +91,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.2-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
-    libhwc2on1adapter
+    gralloc.exynos5 \
+    libhwc2on1adapter \
+    memtrack.exynos5
 
 PRODUCT_PACKAGES += \
     libtinyxml
