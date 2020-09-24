0f3730944a2b01c44d3000a8f047f866037cfee8
followmsi <followmsi@gmail.com>
2020-09-24 16:59:05 +0200

    gtaxl: rootdir: Switch LOCAL_MODULE_CLASS

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -16,35 +16,35 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.samsungexynos7870
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := fstab.samsungexynos7870
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsung.rc
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.samsung.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsungexynos7870.rc
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.samsungexynos7870.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsungexynos7870.usb.rc
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.samsungexynos7870.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.wifi.rc
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.wifi.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
@@ -52,14 +52,14 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.samsungexynos7870.rc
 LOCAL_MODULE_STEM  := ueventd.rc
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := ueventd.samsungexynos7870.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.power.rc
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.power.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
