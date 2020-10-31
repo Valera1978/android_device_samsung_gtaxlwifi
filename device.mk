5891d9740cbffe34beef6132421b74bf22039dcc
followmsi <followmsi@gmail.com>
2020-10-31 16:06:54 +0100

    Revert "gtaxl: revert "revert "Revert "update graphics"""."
    This reverts commit 9f81c63679836471e055ec74b88d9d48d5c62ef2.

--- device.mk
+++ device.mk
@@ -91,14 +91,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.2-service \
+    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
-    gralloc.exynos5 \
-    libhwc2on1adapter \
-    memtrack.exynos5
+    libhwc2on1adapter
 
 PRODUCT_PACKAGES += \
     libtinyxml
