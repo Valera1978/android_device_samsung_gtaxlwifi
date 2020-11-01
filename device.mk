03510a898192700edde3591a37b6768627d61dd1
TALU <taluemail272@gmail.com>
2020-11-01 16:13:41 +0000

    gtaxl: add android.hardware.audio@5.0 and reorder android.hardware.audio.effect@2.0-impl.

--- device.mk
+++ device.mk
@@ -200,10 +200,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@5.0 \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.common@5.0 \
     android.hardware.audio.common@5.0-util \
-    android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
     audio.a2dp.default \
