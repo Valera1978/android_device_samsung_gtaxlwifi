61ba73a7f375ed0b27b2096daf84dda7d46acb24
TALU <taluemail272@gmail.com>
2020-11-28 11:05:33 +0000

    gtaxl: remove unnecessary compatibility_matrix.xml.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,6 @@
 
 LOCAL_PATH := device/samsung/gtaxlwifi
 
-# temporary
 BUILD_BROKEN_DUP_RULES := true
 
 # Include path
@@ -208,7 +207,6 @@ BOARD_VENDOR := samsung
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(LOCAL_PATH)/compatibility_matrix.xml
 
 # Release tools
 TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
