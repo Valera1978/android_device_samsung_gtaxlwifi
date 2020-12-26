e65aae199ea28930f321793c171052f9d5cee082
TALU <taluemail272@gmail.com>
2020-12-26 20:03:44 +0000

    gtaxl: camera: remove the custom camera provider HAL.
    There is no need for us to have this in this device tree. We can simply
    build it from hardware/interfaces/camera/provider/2.4/default/ as-is.
    
    Also, binderize android.hardware.camera.provider.

--- device.mk
+++ device.mk
@@ -153,7 +153,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl.exynos7870 \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.universal7870
 
