072100b163930239ceaf502bfe7c5cda1d1d5067
followmsi <followmsi@gmail.com>
2020-12-28 20:45:40 +0100

    gtaxl: Build libprotobuf-cpp-lite-vendorcompat
    Required for new DRM service
    Remove libprotobuf-cpp-lite-v29.so - not used anymore

--- device.mk
+++ device.mk
@@ -131,8 +131,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service-lazy \
     android.hardware.drm@1.3-service-lazy.clearkey
 
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-lite-vendorcompat
 
 # Keymaster
 PRODUCT_PACKAGES += \
