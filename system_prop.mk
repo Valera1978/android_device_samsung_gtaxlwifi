967e8d41fefcd1c70a4c6f5c0f4b6973a4ff9aa8
followmsi <followmsi@gmail.com>
2021-03-16 16:28:56 +0100

    Revert "gtaxl: system_prop: remove userspace lmkd properties."
    This reverts commit 615cb8d95ef0e2b49f94369304c0716d54548ac4.

--- system_prop.mk
+++ system_prop.mk
@@ -21,6 +21,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
 
+# Options for userspace lmkd
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
+
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
