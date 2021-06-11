8b5cbb1e3ad4ab2d5e96110159e197c2e8761e5f
TALU <taluemail272@gmail.com>
2021-06-11 01:12:55 +0100

    gtaxl: upgrade the health HAL to 2.1.
    Change-Id: I690a796e33f1a49a264280c345f50198ba0ce7c9

--- device.mk
+++ device.mk
@@ -250,10 +250,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
-# Healthd
+# Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 # GPS
 PRODUCT_COPY_FILES += \
