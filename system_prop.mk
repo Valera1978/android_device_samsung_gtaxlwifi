ab55b4902485a19615f7191c2c2e659290cf94e6
TALU <taluemail272@gmail.com>
2021-11-13 23:01:12 +0000

    gtaxl: replace the NUM_FRAMEBUFFER_SURFACE_BUFFERS ConfigStore flag with the corresponding system property
    Reference: https://source.android.com/devices/architecture/configuration
    
    Change-Id: I9f52ff3008fef7b217fb8fff11a9a5fddb91197b

--- system_prop.mk
+++ system_prop.mk
@@ -37,6 +37,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
     debug.sf.disable_backpressure=1
 
+# SurfaceFlinger
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+
 # Lockscreen rotation
 PRODUCT_PROPERTY_OVERRIDES += \
     lockscreen.rot_override=true
