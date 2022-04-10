94b63e5973fec1d203dedc5f8b75d6935f42ff13
TALU <taluemail272@gmail.com>
2022-04-10 13:13:36 +0100

    gtaxl: Get rid of all Exynos power HAL stuff
    An FB notifier callback in the touchscreen driver is now taking care of
    the only functionality that the Exynos power HAL was ever useful for,
    so just get rid of everything for it.
    
    Change-Id: I5a8859d726a88dacb32e65d6c1a8f701fda276ec

--- device.mk
+++ device.mk
@@ -237,8 +237,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power.stats@1.0-service.mock \
-    android.hardware.power@1.0-service.exynos
+    android.hardware.power.stats@1.0-service.mock
 
 # Lights
 PRODUCT_PACKAGES += \
