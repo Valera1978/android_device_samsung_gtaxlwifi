376c250d5627bf0ad31ad4ea464d267480078ce3
TALU <taluemail272@gmail.com>
2022-09-24 23:27:15 +0100

    gtaxlwifi: Stop overriding gps_debug.conf with gps.conf
    gps.conf is now bind mounted to gps_debug.conf instead.
    
    Change-Id: Ie4fb1190a629d41bd5c841ef2a7e8fe462d4f7e1

--- device.mk
+++ device.mk
@@ -226,7 +226,6 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/gps_debug.conf \
     $(LOCAL_PATH)/configs/gps/gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gps.xml
 
 PRODUCT_PACKAGES += \
