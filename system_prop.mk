23059ee37c99fec85553f97766ace3e9c91d12d5
TALU <taluemail272@gmail.com>
2022-10-15 07:56:04 +0100

    gtaxl-common: Set ro.sf.blurs_are_expensive=1
    Change-Id: Ieb1e849706fce8a51509c0f2aca3160bc24dd3b5

--- system_prop.mk
+++ system_prop.mk
@@ -34,6 +34,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     debug.hwc.winupdate=1 \
+    ro.sf.blurs_are_expensive=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.disable_backpressure=1
